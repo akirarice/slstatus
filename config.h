26459d18630e935566887ea96200b0bc99f3743f
Akira <akira@akiranakano.xyz>
2021-12-29 14:05:16 +0100

    add spotify

--- config.h
+++ config.h
@@ -65,9 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ battery_perc, " %s%%|",	    "BAT0"},
-	{ temp	      , "%s°C|",	"/sys/class/thermal/thermal_zone1/temp"},
-	{ wifi_essid, "%s|",    "wlp3s0"},
-	{ vol_perc, "%s%%|",	    "/dev/audio"},
-	{ datetime, "%s ",           "%m/%d %R" },
+	{ datetime, " %s ",           "%m/%d %R" },
+	{ run_command, " %s | Spotify:",	      "echo 'Vol:' $(pamixer --get-volume-human)" },
+	{ run_command, " %s ",	      "spt playback -d spotify-tui -f '%s %t - %a'" },	
 };
